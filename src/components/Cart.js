c86312ee8be4b85fa2428d9f452185a4e1e708da
DavidBotina09 <davidbotinasc@gmail.com>
2022-10-25 23:28:36 -0500

    agregando boton terminar compra

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,11 +1,13 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import { useCardContext } from "./CartContext";
 import ItemCart from "./ItemCart";
 import "./Cart.css"
+import { Button } from "react-bootstrap";
 
 const Cart = () => {
     const {cart , totalPrice } = useCardContext();
+
+
 
     if(cart.length === 0){
         return(
@@ -17,6 +19,8 @@ const Cart = () => {
         );
     }
 
+    
+
     return ( 
         <>
         <h2>bienvenido al carrito del compras</h2> 
@@ -24,7 +28,8 @@ const Cart = () => {
         {cart.map(product => <ItemCart key={product.id} product={product} />)}  
         </div>
         
-        <p> PrecioTotal: {totalPrice()}</p>     
+        <p> PrecioTotal: {totalPrice()}</p>  
+        <Button variant="success">Finalizar compra</Button>   
         </>
 
      );
