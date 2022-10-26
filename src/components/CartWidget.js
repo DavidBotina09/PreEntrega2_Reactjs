c252e1ea3cc5e28f0c691661c84a126985cca556
DavidBotina09 <davidbotinasc@gmail.com>
2022-10-25 20:04:54 -0500

    agregando numero de productos en carrito

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -1,13 +1,19 @@
 import { FaShoppingCart } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import './CartWidget.css';
+import { useCardContext } from './CartContext';
 
 const CartWidget = () => {
+    const {totalProducts} = useCardContext();
     //agregar icono
     return (
+        <>
         <Link to="/cart">
         <FaShoppingCart />
         </Link>
+        <span className='numero'>{totalProducts() || " "}</span>
+        </>
+
         
     );
 }
